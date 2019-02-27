b0a39018383a13c8643d52d26b6f35212be19180
Katja Süss <k.suess@rohberg.ch>
2019-02-27 17:55:04 +0100

    Fix of Travis configuration

--- setup.py
+++ setup.py
@@ -18,15 +18,20 @@ setup(
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
+        "Topic :: Multimedia :: Sound/Audio :: Players",
+        'Development Status :: 5 - Production/Stable',
         "Environment :: Web Environment",
         "Framework :: Plone",
         "Framework :: Plone :: 5.1",
+        "Framework :: Plone :: 5.2",
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
         "Operating System :: OS Independent",
         "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
     ],
-    keywords='Python Plone',
+    keywords='audio player playlist',
     author='ksuess',
     author_email='k.suess@rohberg.ch',
     url='https://pypi.python.org/pypi/collective.playlist',
@@ -37,7 +42,6 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        # -*- Extra requirements: -*-
         'plone.app.dexterity',
         'plone.api',
         'Products.GenericSetup>=1.8.2',
@@ -47,11 +51,7 @@ setup(
     extras_require={
         'test': [
             'plone.app.testing',
-            # Plone KGS does not use this version, because it would break
-            # Remove if your package shall be part of coredev.
-            # plone_coredev tests as of 2016-04-01.
             'plone.testing>=5.0.0',
-            'plone.app.contenttypes',
             'plone.app.robotframework[debug]',
         ],
     },
