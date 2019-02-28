3eaafab81a0fdceaebf55ba347efb4248b228002
Katja Süss <k.suess@rohberg.ch>
2019-02-28 12:53:11 +0100

    Travis Chrome, not Firefox

--- src/collective/playlist/tests/test_setup.py
+++ src/collective/playlist/tests/test_setup.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 """Setup tests for this package."""
 from collective.playlist.testing import COLLECTIVE_PLAYLIST_INTEGRATION_TESTING  # noqa
-from plone import api
 from plone.app.testing import setRoles
 from plone.app.testing import TEST_USER_ID
 
